584769f02646a7040dd70aca6c293e5fca3e64ef
Aarnav Bos <aarnavbos@gmail.com>
2018-07-26 13:19:57 -0400

    separated parser functions, switched to phantomjs for a better outcome

--- index.js
+++ index.js
@@ -1,115 +1,45 @@
-const wkhtmltopdf = require('wkhtmltopdf');
+const phantom = require('phantom')
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const fs = require('fs')
+const Parser = require('rss-parser');
+const IPFS = require('ipfs-api')
+const parser = require('./parse')
 
-const html = fs.readFileSync('article3.html').toString()
-const $ = cheerio.load(html)
-
-// function to add 'https://corrective.org' as a prefix because links in the webpage are relative
-function prefixAttribute(_this, attr){
-  if (_this.attr(attr)[0] == '/'){
-    let url = 'https://correctiv.org'+_this.attr(attr)
-    _this.attr(attr, url)
-  }
-}
-
-// begin parsing the article
-// remove:
-// all scripts
-// modify all URLS by adding a prefix of the domain where needed
-// remove suggested articles
-// remove navbars
-// remove comment form
-// remove related
-
-$('a').each(function(i, elem){
-  let _this = $(this)
-  prefixAttribute(_this, 'href')
-})
-$('img').each(function(i, elem){
-  _this = $(this)
-  prefixAttribute(_this, 'src')
-})
-$('source').each(function(i, elem){
-  _this = $(this)
-  prefixAttribute(_this, 'srcset')
-})
-
-$('link').each(function(i, elem){
-  _this = $(this)
-  prefixAttribute(_this, 'href')
-})
+let rssParser = new Parser();
+const ipfs = IPFS('127.0.0.1', '5001',{protocol:'http'})
 
 
-// remove all scripts and noscript tags
-$('script').each(function(i, elem){
-  $(this).remove()
-})
-$('noscript').each(function(i, elem){
-  $(this).remove()
-})
-
-// remove all navbars
-$('nav').each(function(i, elem){
-  $(this).remove()
-})
-$('.nav-primary').remove()
-
-// remove all forms
-$('form').each(function(i, elem){
-  $(this).remove()
-})
-
-// individual elements to remove
-const to_rm = [
-  // remove footer meta
-  '.footer__meta',
-  // remove suggested articles
-  '.article-body__calltoaction',
-  // remove login link
-  '.footer__account-login',
-  // promo banner
-  '.promo-banner',
-  // comments
-  '#comment-list',
-  '.article-footer__header',
-  // remove call out
-  '.callout'
-]
-
-to_rm.forEach(item => {
-  $(item).remove()
-})
-
-
-
-$('.footer__language').attr('align', 'center')
-
-// cleanup links showing the URL in the pdf and add some page breaking rules
-$('head').append(
-  `
-    <style>
-    a::after {
-     content: "" !important;
-     text-decoration: underline !important;
-    }
-    body{
-      page-break-inside: avoid !important;
-    }
-    </style>
-      `
-)
-$('body').css('page-break-inside', 'avoid !imporant')
-
-
-const write = () => {
+const sm = () => {
   return new Promise(async (resolve, reject) => {
-    await fs.writeFileSync('something.html', Buffer.from($.html()))
-    resolve(123)
-  });
-}
+    const feed = await rssParser.parseURL('https://correctiv.org/artikel/feeds/')
+    const keys = Object.keys(feed.items[0])
+    resolve(feed.items[0].link)
 
-write().then(data => {
-  var stream = wkhtmltopdf(fs.createReadStream('something.html'), {logLevel: 'error'}).pipe(fs.createWriteStream('out.pdf'))
+  })
+
+  // reverse to get oldest first
+  //feed.items.reverse().forEach(item => {
+    // /console.log(item.title + ':' + item.link)
+  //});
+}
+sm().then(async(article) => {
+  const res = await fetch(article)
+  const html = await res.text()
+  console.log(html)
+  const $ = cheerio.load(html)
+  parser.parseHTML($)
+  parser.writeHTML($).then(data => {
+    // create PDF
+    phantom.create().then(instance => {
+      instance.createPage().then(async(page) => {
+        await page.property('viewportSize', { width: 600, height: 800 })
+        await page.property('paperSize',{format: 'A3', orientation: 'portrait'})
+        //const _ = await fs.readFileSync('./something.html').toString()
+        await page.open('./something.html')
+        await page.render('out.pdf')
+        instance.exit()
+      })
+    })
+  })
 })
---
+++ parse.js
@@ -0,0 +1,113 @@
+const fs = require('fs')
+// function to add 'https://corrective.org' as a prefix because links in the webpage are relative
+function prefixAttribute(_this, attr){
+  if (_this.attr(attr)[0] == '/'){
+    let url = 'https://correctiv.org'+_this.attr(attr)
+    _this.attr(attr, url)
+  }
+}
+
+parseLinks = ($) => {
+  $('a').each(function(i, elem){
+    let _this = $(this)
+    prefixAttribute(_this, 'href')
+  })
+  $('img').each(function(i, elem){
+    _this = $(this)
+    prefixAttribute(_this, 'src')
+  })
+  $('source').each(function(i, elem){
+    _this = $(this)
+    prefixAttribute(_this, 'srcset')
+  })
+
+  $('link').each(function(i, elem){
+    _this = $(this)
+    prefixAttribute(_this, 'href')
+  })
+  return $
+}
+
+
+// remove:
+// all scripts
+// modify all URLS by adding a prefix of the domain where needed
+// remove suggested articles
+// remove navbars
+// remove comment form
+// remove related
+
+const removeElements = ($) => {
+  // remove all scripts and noscript tags
+  $('script').each(function(i, elem){
+    $(this).remove()
+  })
+  $('noscript').each(function(i, elem){
+    $(this).remove()
+  })
+
+  // remove all navbars
+  $('nav').each(function(i, elem){
+    $(this).remove()
+  })
+
+  // remove all forms
+  $('form').each(function(i, elem){
+    $(this).remove()
+  })
+
+  // individual elements to remove
+  const to_rm = [
+    // remove footer meta
+    '.footer__meta',
+    // remove suggested articles
+    '.article-body__calltoaction',
+    // remove login link
+    '.footer__account-login',
+    // promo banner
+    '.promo-banner',
+    // comments
+    '#comment-list',
+    '.article-footer__header',
+    // remove call out
+    '.callout',
+    // nav
+    '.nav-primary',
+  ]
+
+  to_rm.forEach(item => {
+    $(item).remove()
+  })
+}
+
+const rePosition = ($) => {
+  $('.footer__language').attr('align', 'center')
+
+  // cleanup links showing the URL in the pdf and add some page breaking rules
+  $('head').append(
+    `
+      <style>
+      a::after {
+       content: "" !important;
+       text-decoration: underline !important;
+      }
+      body{
+        page-break-inside: avoid !important;
+      }
+      </style>
+        `
+  )
+  $('body').css('page-break-inside', 'avoid !imporant')
+}
+exports.parseHTML = ($) => {
+    parseLinks($)
+    removeElements($)
+    rePosition($)
+    return $
+}
+exports.writeHTML = ($) => {
+  return new Promise(async (resolve, reject) => {
+    await fs.writeFileSync('something.html', Buffer.from($.html()))
+    resolve()
+})
+}
